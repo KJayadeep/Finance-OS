4601137f6b455c8a2192d5757a3a5e482f743c00
jayadeep <jayadeep988@gmail.com>
2026-08-29 22:17:50 +0530

    created route for update profile

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signup,login,checkAuth,logout} from "../controllers/auth.js";
+import { signup,login,checkAuth,logout,updateProfile} from "../controllers/auth.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const authRouter = Router();
@@ -8,6 +8,7 @@ authRouter.post("/signup",signup)
 authRouter.post("/login",login)
 authRouter.post("/logout",logout)
 authRouter.get("/check-auth",protect,checkAuth)
+authRouter.post("/update-profile",protect,updateProfile)
 
 
 export default authRouter;
