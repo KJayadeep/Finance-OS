3502d8483ea9fc5ac43385f9dc7530eeb1ebf181
jayadeep <jayadeep988@gmail.com>
2026-08-29 12:24:01 +0530

    connect to redis

--- Backend/config/redis.js
+++ Backend/config/redis.js
@@ -3,7 +3,7 @@ import Redis from "ioredis";
 const redis = new Redis(process.env.REDIS_URL)
 
 redis.on("connect", () => {
-  console.log(`Redis connected`);
+  console.log(`Redis connected:`);
 });
 
 redis.on("error", (error) => {
