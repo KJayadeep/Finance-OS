e7c6ca204db3c612a99bad2c9e513e4e2e9987cf
jayadeep <jayadeep988@gmail.com>
2026-09-03 19:02:53 +0530

    fix nodemailer

--- Backend/config/nodeMailer.js
+++ Backend/config/nodeMailer.js
@@ -2,9 +2,7 @@ import nodemailer from "nodemailer";
 import "dotenv/config";
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
